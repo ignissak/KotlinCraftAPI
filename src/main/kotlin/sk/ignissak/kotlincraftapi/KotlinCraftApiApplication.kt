8d5a21935b7cb24fc3e239ad854cbe3a6e801803
Jakub Bordáš <methalicity@gmail.com>
2021-11-06 14:59:37 +0100

    Init

---
+++ src/main/kotlin/sk/ignissak/kotlincraftapi/KotlinCraftApiApplication.kt
@@ -0,0 +1,11 @@
+package sk.ignissak.kotlincraftapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinCraftApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinCraftApiApplication>(*args)
+}
